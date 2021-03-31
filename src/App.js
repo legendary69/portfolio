19f5859cc2706d6dfef9cbb6fa83fef87bf25b8c
unknown <anshumansingh758@gmail.com>
2021-03-31 16:45:17 +0530

    started work on homepage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import './App.scss';
 import Navbar from './Components/Navbar';
 import HomePage from './Pages/HomePage';
+import AboutPage from './Pages/AboutPage';
+import PortfolioPage from './Pages/PortfolioPage';
+import BlogPage from './Pages/BlogPage';
+import ContactPage from './Pages/ContactPage';
+import {Switch, Route} from 'react-router-dom';
 
 function App() {
   	return (
@@ -10,7 +15,23 @@ function App() {
 			</div>
 			<div className="main-content">
 				<div className="content">
-					<HomePage />
+					<Switch>
+						<Route path="/" exact>
+							<HomePage />
+						</Route>
+						<Route path="/about" exact>
+							<AboutPage />
+						</Route>
+						<Route path="/portfolio" exact>
+							<PortfolioPage />
+						</Route>
+						<Route path="/blogs" exact>
+							<BlogPage />
+						</Route>
+						<Route path="/contact" exact>
+							<ContactPage />
+						</Route>
+					</Switch>
 				</div>  
 		  	</div>
 		</div>
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,4 +1,11 @@
 import React from 'react';
+import {faFacebook} from '@fortawesome/free-brands-svg-icons';
+import {faGithub} from '@fortawesome/free-brands-svg-icons';
+import {faYoutube} from '@fortawesome/free-brands-svg-icons';
+import {faInstagram} from '@fortawesome/free-brands-svg-icons';
+import {faTwitter} from '@fortawesome/free-brands-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {Link} from 'react-router-dom';
 
 function HomePage() {
     return (
@@ -6,8 +13,28 @@ function HomePage() {
             <header className="hero">
                 <h1 className="hero-text">
                     Hi, I am
-                    <span>Singh Anshuman.</span>
+                    <span> Singh Anshuman.</span>
                 </h1>
+                <p className="h-sub-text">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                </p>
+                <div className="icons">
+                    <Link className="icon-holder">
+                        <FontAwesomeIcon icon={faFacebook} className="icon fb"/>
+                    </Link>
+                    <Link className="icon-holder">
+                        <FontAwesomeIcon icon={faInstagram} className="icon inst"/>
+                    </Link>
+                    <Link className="icon-holder">
+                        <FontAwesomeIcon icon={faTwitter} className="icon tw"/>
+                    </Link>
+                    <Link className="icon-holder">
+                        <FontAwesomeIcon icon={faGithub} className="icon gh"/>
+                    </Link>
+                    <Link className="icon-holder">
+                        <FontAwesomeIcon icon={faYoutube} className="icon yt"/>
+                    </Link>
+                </div>
             </header>
         </div>
     )
