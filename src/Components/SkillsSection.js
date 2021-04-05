492fbb010a51867c86e81bec411a92dbf295f187
unknown <anshumansingh758@gmail.com>
2021-04-05 10:32:42 +0530

    started work on skill section.

---
+++ src/Components/SkillsSection.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function SkillsSection() {
+    return (
+        <div className="SkillsSection">
+            <div className="skills-container">
+                <h5 className="skill-title"></h5>
+                <div className="skill-bar">
+                    <p className="skill-text"></p>
+                    <div className="skill-progress">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SkillsSection
